bbbcf54cc8a68e16b25a3dabdc904cb9ef6a9ca1
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2020-12-22 17:58:02 +0530

    Feat: Add generic calculation
    Calculation of number of denominations can now be done using user input

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-let currencyValues = [2000,500,100,20,10,5,1]
-let bill_amt = 243, cash_amt = 2000
-let change_amt = cash_amt - bill_amt
+const inputBill = document.querySelector('input[name=bill_amt]')
+const inputCash = document.querySelector('input[name=cash_amt]')
+const btnCalculate = document.querySelector('#btn_calculate')
+const paraElement = document.querySelector('#para')
 
-let currencyMap = new Map()
-currencyValues.forEach(value =>{
-    if(change_amt >0){
-        //notes count of each denominations
-        let notesCount = Math.floor(change_amt / value);
-        currencyMap.set(value , notesCount);
-        // remaining change value
-        change_amt = change_amt % value;
-    }
+function calculateMinNotes(bill , cashGiven){
+    
+    let currencyValues = [2000,500,100,20,10,5,1]
+    let change_amt = cashGiven - bill
+    
+    let currencyMap = new Map()
+    currencyValues.forEach(value =>{
+        if(change_amt >0){
+            //notes count of each denominations
+            let notesCount = Math.floor(change_amt / value);
+            currencyMap.set(value, notesCount );
+            // remaining change value
+            change_amt = change_amt % value;
+        }
+    })
+
+    displayResults(currencyMap);
+}
+
+function displayResults(currencyMap){
+    let resultStr = ``;
+    currencyMap.forEach((notesCount , value)=>{
+        resultStr += `Denomination: ${value}  Notes Count: ${notesCount}\n`
+    })
+    paraElement.innerText = resultStr;
+}
+
+btnCalculate.addEventListener('click',()=>{
+    calculateMinNotes(inputBill.value , inputCash.value)
 })
